4e3b2b73cb8a2edd2f8dacf84f02374bd86bd049
Oussama Loukili Idrissi <oussamaloukili9113@gmail.com>
2023-09-24 14:09:06 +0100

    Adds missing check for admin role in isOperatorEmployee

--- app/middlewares/authJwt.js
+++ app/middlewares/authJwt.js
@@ -73,7 +73,7 @@ isOperatorEmployee = (req, res, next) => {
         }
 
         for (let i = 0; i < roles.length; i++) {
-          if (roles[i].name === "operatorEmployee") {
+          if (roles[i].name === "operatorEmployee" || roles[i].name === "admin") {
             next();
             return;
           }
